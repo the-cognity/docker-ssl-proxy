da75e21c4f7d5227ff25329de7613cc542648215
Rashair <przemyslaw.wk@gmail.com>
2023-01-08 15:48:01 +0100

    Fix line endings

--- configure_and_start.sh
+++ configure_and_start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
+echo 'here1'
 export SERVER_NAME=${SERVER_NAME:-localhost}
+echo 'here2'
 export TARGET_SCHEME=${TARGET_SCHEME:-http}
 export TARGET_PORT=${TARGET_PORT:-80}
 export TARGET_HOST=${TARGET_HOST:-proxyapp}
@@ -8,9 +10,13 @@ export TARGET_HOST_HEADER=${TARGET_HOST_HEADER:-${TARGET_HOST}}
 export CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-20M}
 export SSL_PORT=${SSL_PORT:-443}
 
+echo 'here6'
 REPLACEABLE='$SERVER_NAME:$TARGET_SCHEME:$TARGET_PORT:$TARGET_HOST:$TARGET_HOST_HEADER:$CLIENT_MAX_BODY_SIZE:$SSL_PORT'
 
+echo 'here8'
 envsubst $REPLACEABLE </nginx.conf.template >/etc/nginx/nginx.conf
+
+echo 'here100'
 
 /add_self_signed_certs.sh
 
